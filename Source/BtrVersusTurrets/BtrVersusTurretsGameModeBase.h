156cb1810f80b4a9587c600096fc8fa9147101a7
vipe4ka-visko4ka <pavel.makk.old@gmail.com>
2022-11-12 07:52:23 +0300

    Init

---
+++ Source/BtrVersusTurrets/BtrVersusTurretsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BtrVersusTurretsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BTRVERSUSTURRETS_API ABtrVersusTurretsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
